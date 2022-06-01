a2318829ba0d73ed5e6d2774ed3010364a781536
JPN20 <75049978+JPN20@users.noreply.github.com>
2022-06-01 19:03:50 -0300

    Update PessoaJuridica.cs

--- classes/PessoaJuridica.cs
+++ classes/PessoaJuridica.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using Curso.Interfaces;
 
 namespace Curso.Classes
@@ -24,16 +25,87 @@ namespace Curso.Classes
             throw new NotImplementedException();
         }
 
-        public bool ValidarCnpj(string cnpj)
+        public bool ValidarCnpj(string? cnpj)
         {
-            throw new NotImplementedException();
+
+            int[] multiplicador1 = new int[12] { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[13] { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int somatorio;
+            int resto;
+            string digito;
+            string cnpjAux;
+
+            try
+            {
+                if (String.IsNullOrEmpty(cnpj))
+                    return false;
+
+                cnpj = cnpj.Trim();
+
+                if (Regex.IsMatch(cnpj, @"((\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2})|(\d{14}))"))
+                {
+
+                    if (cnpj.Length == 18)
+                        cnpj = cnpj.Replace(".", "").Replace("/", "").Replace("-", "");
+
+                    if (cnpj.All(c => c.Equals(cnpj.First())))
+                        return false;
+
+                    somatorio = 0;
+
+                    cnpjAux = cnpj.Substring(0, 12);
+
+                    for (int i = 0; i < cnpjAux.Length; i++)
+                        somatorio += int.Parse(cnpjAux[i].ToString()) * multiplicador1[i];
+
+                    resto = somatorio % 11;
+
+                    if (resto < 2)
+                        resto = 0;
+                    else
+                        resto = 11 - resto;
+
+                    digito = resto.ToString();
+                    cnpjAux = cnpjAux + digito;
+
+                    somatorio = 0;
+
+                    for (int i = 0; i < cnpjAux.Length; i++)
+                        somatorio += int.Parse(cnpjAux[i].ToString()) * multiplicador2[i];
+
+                    resto = somatorio % 11;
+
+                    if (resto < 2)
+                        resto = 0;
+                    else
+                        resto = 11 - resto;
+
+                    digito = digito + resto.ToString();
+
+                }
+                else
+                    return false;
+
+                return cnpj.EndsWith(digito);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
         }
 
         public override string ToString()
         {
-            return base.ToString() 
+            string cnpjValido = "";
+
+            if (!String.IsNullOrEmpty(Cnpj))
+                cnpjValido = ValidarCnpj(Cnpj) ? "Sim" : "Não";
+
+            return base.ToString()
             + "CNPJ: " + Cnpj
             + "\nRazão Social: " + RazaoSocial
+            + "\nCNPJ válido: " + cnpjValido
             + "\n";
         }
     }
