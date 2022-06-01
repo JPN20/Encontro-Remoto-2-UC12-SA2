b3f0e8a7d2d884382f65fd1e2dbaef9ce300d139
JPN20 <75049978+JPN20@users.noreply.github.com>
2022-06-01 18:46:52 -0300

    Update Pessoa.cs

--- classes/Pessoa.cs
+++ classes/Pessoa.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using Curso.Interfaces;
 
 namespace Curso.Classes
@@ -5,10 +6,31 @@ namespace Curso.Classes
     public abstract class Pessoa: IPessoa
     {
         public string? Nome { get; set; }
-        public string? Endereco { get; set; }
+        public Endereco? Endereco { get; set; }
         public float? Rendimento { get; set; }
 
+        public Pessoa()
+        {
+        }
+
+        public Pessoa(string nome, Endereco endereco, float rendimento)
+        {
+            Nome = nome;
+            Endereco = endereco;
+            Rendimento = rendimento;
+        }
         public abstract float PagarImposto(float rendimento);
+
+        public override string ToString()
+        { 
+            StringBuilder sb = new StringBuilder();
+            
+            sb.AppendLine("Nome: " + Nome);
+            sb.AppendLine(Endereco?.ToString());
+            sb.AppendLine("Rendimento: " + Rendimento?.ToString("C"));
+
+            return sb.ToString();
+        }
     
     }
 }
