7d36a0e6b6269595a9e5b2773288baf19fdbba4c
JPN20 <75049978+JPN20@users.noreply.github.com>
2022-06-01 18:47:56 -0300

    Update PessoaJuridica.cs

--- classes/PessoaJuridica.cs
+++ classes/PessoaJuridica.cs
@@ -8,6 +8,17 @@ namespace Curso.Classes
         public string? Cnpj { get; set; }
         public string? RazaoSocial { get; set; }
 
+        public PessoaJuridica()
+        {
+        }
+
+        public PessoaJuridica(string nome, Endereco endereco, float rendimento, string cnpj,
+        string razaoSocial) : base(nome, endereco, rendimento)
+        {
+            Cnpj = cnpj;
+            RazaoSocial = razaoSocial;
+        }
+
         public override float PagarImposto(float rendimento)
         {
             throw new NotImplementedException();
@@ -16,6 +27,14 @@ namespace Curso.Classes
         public bool ValidarCnpj(string cnpj)
         {
             throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() 
+            + "CNPJ: " + Cnpj
+            + "\nRazão Social: " + RazaoSocial
+            + "\n";
         }
     }
 }
