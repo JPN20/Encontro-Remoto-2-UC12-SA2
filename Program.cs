03d163d6e5141a4f6fdb4a933ecd667de0709222
JPN20 <75049978+JPN20@users.noreply.github.com>
2022-06-01 18:38:13 -0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,8 @@
+// See https://aka.ms/new-console-template for more information
+
+using Curso.Classes;
+
+PessoaFisica pf = new PessoaFisica();
+pf.Nome = "Leandro";
+
+Console.WriteLine($"Nome: { pf.Nome }");
